a4da353c87108ab1e83f02ab7eebc55ae74fe45f
Hamilton Gabriel <gabrielzinho.gabriel2@gmail.com>
2020-04-28 09:11:00 -0400

    clean code and add class user in an extra file

--- user.spec.js
+++ user.spec.js
@@ -1,18 +1,10 @@
-class UserSpec {
-  constructor (details) {
-    const { firstname, lastname } = details
-    this.firstname = firstname
-    this.lastname = lastname
-  }
-
-  get name () {
-    return `${this.firstname} ${this.lastname}`
-  }
-}
-
-describe('User', () => {
-  test('name returns full name', () => {
-    const user = new UserSpec({ firstname: 'Hamilton', lastname: 'Gabriel' })
-    expect(user.name).toBe('Hamilton Gabriel')
+import UserSpec from "./user"
+describe("User", () => {
+  test("name returns full name", () => {
+    const user = new UserSpec({
+      firstname: "Hamilton Gabriel",
+      lastname: "Pinheiro",
+    })
+    expect(user.name).toBe("Hamilton Gabriel Pinheiro")
   })
 })
